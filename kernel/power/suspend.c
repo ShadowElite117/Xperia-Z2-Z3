ad03fd4f9a1aa533ec9001bc5d93006fa7f2a034
Zhang Rui <rui.zhang@intel.com>
2013-03-27 03:36:10 +0000

    PM / sleep: add TEST_PLATFORM support for freeze state
    Invoke freeze_enter() after suspend_test(TEST_PLATFORM) being invoked.
    
    So when setting /sys/power/pm_test to "platform", it can be used to
    check if freeze state is working well after all devices are suspended
    and before processors are blocked,
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Luis <lefelite@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -187,6 +187,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 			goto Platform_wake;
 	}
 
+	if (suspend_test(TEST_PLATFORM))
+		goto Platform_wake;
+
 	/*
 	 * PM_SUSPEND_FREEZE equals
 	 * frozen processes + suspended devices + idle processors.
@@ -197,9 +200,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		freeze_enter();
 		goto Platform_wake;
 	}
-
-	if (suspend_test(TEST_PLATFORM))
-		goto Platform_wake;
 
 	error = disable_nonboot_cpus();
 	if (error || suspend_test(TEST_CPUS))
