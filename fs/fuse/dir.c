cee0329907ccf4b343454921a24f67ecc05d7b8f
Luis <lefelite@gmail.com>
2019-06-12 02:45:56 -0500

    Updated linux kernel version 3.4.62 -> 3.4.63

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1519,6 +1519,8 @@ static int fuse_setxattr(struct dentry *entry, const char *name,
 		fc->no_setxattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
@@ -1648,6 +1650,8 @@ static int fuse_removexattr(struct dentry *entry, const char *name)
 		fc->no_removexattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
