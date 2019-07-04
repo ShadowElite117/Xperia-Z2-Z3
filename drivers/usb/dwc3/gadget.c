af15071a170e9a4548634636e4d7b58bd6cdc996
Luis <lefelite@gmail.com>
2019-07-03 19:25:49 -0500

    Updated linux kernel version 3.4.106 -> 3.4.107

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1036,6 +1036,8 @@ static void dwc3_prepare_trbs(struct dwc3_ep *dep, bool starting)
 					break;
 			}
 			dbg_queue(dep->number, &req->request, 0);
+			if (last_one)
+				break;
 		} else {
 			struct dwc3_request	*req1;
 			int maxpkt_size = usb_endpoint_maxp(dep->endpoint.desc);
